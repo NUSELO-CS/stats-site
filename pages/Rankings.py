e6cab726a6bdaea1d98ddf692a3d26704e1d927c
Finn <finnfarrer@yahoo.co.uk>
2025-06-19 00:47:50 +0100

    modification to visuals of ranking page

--- pages/Rankings.py
+++ pages/Rankings.py
@@ -11,29 +11,25 @@ st.title("UKCS Rankings")
 rankings = get_current_rankings()
 
 # Add column headers
-header_cols = st.columns([1, 3, 5, 2])
-with header_cols[0]:
-    st.markdown("**Ranking**")
+header_cols = st.columns([0.1,0.9, 3, 5, 1])
 with header_cols[1]:
-    st.markdown("**Team Name**")
+    st.markdown("**Ranking**")
 with header_cols[2]:
-    st.markdown("**Players**")
-with header_cols[3]:
-    st.markdown("**Points**")
-
-st.markdown("---")
+    st.markdown("**Team Name**")
 
 if not rankings:
     st.markdown("No rankings available.")
 else:
     for team in rankings:
-        cols = st.columns([1, 3, 5, 2])
-        with cols[0]:
-            st.markdown(f"**{team['ranking']}**")
-        with cols[1]:
-            st.markdown(f"**{team['team_name']}**")
-        with cols[2]:
-            st.markdown(" - ".join(team['roster_names']))
-        with cols[3]:
-            st.markdown(f"**{team['points']}**")
-        st.markdown("---")
+        with st.container(border=True):
+            cols = st.columns([0.1, 1, 3, 6.4, 0.5])
+            with cols[0]:
+                st.write("") 
+            with cols[1]:
+                st.markdown(f"**{team['ranking']}**")
+            with cols[2]:
+                st.markdown(f"**{team['team_name']}**")
+            with cols[3]:
+                st.markdown("\u00A0\u00A0-\u00A0\u00A0".join(team['roster_names']))
+            with cols[4]:
+                st.markdown(f"**{team['points']}**")
